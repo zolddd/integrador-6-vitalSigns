6cc7970b1343eea5052ae84dd1de253070465d1c
valie <isauravpr@gmail.com>
2023-11-19 19:32:46 -0600

    probabilidad signos

--- controllers/VitalSigns.js
+++ controllers/VitalSigns.js
@@ -1,67 +1,174 @@
 import { query } from "../connection.js";
 
 export const createVitalSigns = async (req, res) => {
-    try {
-      const { heart_rate,temperature,systolic_pressure, diastolic_pressure,blood_oxygen} = req.body;
-      const sql =
+  try {
+    const {
+      heart_rate,
+      temperature,
+      systolic_pressure,
+      diastolic_pressure,
+      blood_oxygen,
+    } = req.body;
+
+    const sql =
       "INSERT INTO vitalSigns (heart_rate,temperature,systolic_pressure, diastolic_pressure,blood_oxygen) VALUES (?,?,?,?,?)";
-      const params = [heart_rate,temperature,systolic_pressure, diastolic_pressure,blood_oxygen];
-      const [result] = await query(sql, params);
-      res.json({ message: "Save succesfuly!" });
-    } catch (error) {
-      return error;
-    }
+    const params = [
+      heart_rate,
+      temperature,
+      systolic_pressure,
+      diastolic_pressure,
+      blood_oxygen,
+    ];
+    const [result] = await query(sql, params);
+    res.json({ message: "Save succesfuly!" });
+  } catch (error) {
+    return error;
+  }
 };
 
 export const getVitalSigns = async (req, res) => {
-    try {
-      const [result] = await query("SELECT * FROM vitalSigns", []);
-      console.log(result);
-      res.status(201).json(result);
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-};
-
-export const getLastWeek =async (req, res)=>{
   try {
-    const [result] = await query(`SELECT *
-    FROM vitalSigns
-    WHERE DATE(create_at) >= CURDATE() - INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY
-      AND DATE(create_at) <= CURDATE() - INTERVAL (DAYOFWEEK(CURDATE()) - 8) DAY
-      AND DAYOFWEEK(create_at) BETWEEN 2 AND 7`, []);    
+    const [result] = await query("SELECT * FROM vitalSigns", []);
+    console.log(result);
     res.status(201).json(result);
-  } catch (error) {error
+  } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-}
+};
 
-export const probabilisticEvent = async (req, res)=>{
-
+export const probabilisticEvent = async (req, res) => {
   try {
-    const [result] = await query(`SELECT * FROM vitalSigns ORDER BY id DESC
-    LIMIT 1`, []);
-    console.log("imprimiendo: "+result);
+    const [result] = await query(
+      `SELECT * FROM vitalSigns ORDER BY id DESC
+    LIMIT 1`,
+      []
+    );
+
     console.log(result[0].temperature); //acedemos a temperatura
-    const temperaturaMedida = result[0].temperature
+    const temperaturaMedida = result[0].temperature;
     const umbralFiebre = 38; //esta temperatura es considerada fiebre
 
     // Inicializar el número de casos favorables y totales
     const casosFavorables = 0;
-     const casosTotales = 1;
+    const casosTotales = 1;
 
     // Verificar si la temperatura indica fiebre
     if (temperaturaMedida >= umbralFiebre) {
       casosFavorables = 1;
-     }
-
+    }
     // Calcular la probabilidad
     const probabilidadFiebre = casosFavorables / casosTotales;
-    console.log(probabilidadFiebre)
+    console.log(probabilidadFiebre);
 
     res.status(201).json(probabilidadFiebre);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
+};
 
+function calcularPromedio(datos) {
+  // Verificar si hay datos en el arreglo
+  if (datos.length === 0) {
+    console.error("El arreglo está vacío no se ha hecho mediciones");
+    return [];
+  }
+
+  let sumaHeartRate = 0;
+  let sumaTemperature = 0;
+  let sumaSystolicPressure = 0;
+  let sumaDiastolicPressure = 0;
+  let sumaBloodOxygen = 0;
+  let fechaPromedio = 0;
+
+  datos.forEach((dato) => {
+    sumaHeartRate += dato.heart_rate;
+    sumaTemperature += dato.temperature;
+    sumaSystolicPressure += dato.systolic_pressure;
+    sumaDiastolicPressure += dato.diastolic_pressure;
+    sumaBloodOxygen += dato.blood_oxygen;
+    fechaPromedio = dato.create_at;
+  });
+
+  // Calcular promedio dividiendo por (N) la cantidad de datos
+  const cantidadDatos = datos.length;
+  const promedioHeartRate = sumaHeartRate / cantidadDatos;
+  const promedioTemperature = sumaTemperature / cantidadDatos;
+  const promedioSystolicPressure = sumaSystolicPressure / cantidadDatos;
+  const promedioDiastolicPressure = sumaDiastolicPressure / cantidadDatos;
+  const promedioBloodOxygen = sumaBloodOxygen / cantidadDatos;
+
+  return {
+    promedioHeartRate,
+    promedioTemperature,
+    promedioSystolicPressure,
+    promedioDiastolicPressure,
+    promedioBloodOxygen,
+    fechaPromedio,
+  };
 }
+
+export const promedio = async (req, res) => {
+  try {
+    const totalPromedios = [];
+
+    const [result] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 1 DAY`,
+      []
+    );
+
+    let promedio1 = await calcularPromedio(result);
+    totalPromedios.push(promedio1);
+
+    const [result2] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 2 DAY`,
+      []
+    );
+
+    let promedio2 = await calcularPromedio(result2);
+    totalPromedios.push(promedio2);
+
+    const [result3] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 3 DAY`,
+      []
+    );
+
+    let promedio3 = await calcularPromedio(result3);
+    totalPromedios.push(promedio3);
+
+    const [result4] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 4 DAY`,
+      []
+    );
+
+    let promedio4 = await calcularPromedio(result4);
+    totalPromedios.push(promedio4);
+
+    const [result5] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 5 DAY`,
+      []
+    );
+
+    let promedio5 = await calcularPromedio(result5);
+    totalPromedios.push(promedio5);
+
+    const [result6] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 6 DAY`,
+      []
+    );
+
+    let promedio6 = await calcularPromedio(result6);
+    totalPromedios.push(promedio6);
+
+    const [result7] = await query(
+      `SELECT * FROM vitalSigns WHERE create_at = CURDATE() - INTERVAL 7 DAY`,
+      []
+    );
+
+    let promedio7 = await calcularPromedio(result7);
+    totalPromedios.push(promedio7);
+
+    res.json({ totalPromedios });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- routers/vitalsignsRouter.js
+++ routers/vitalsignsRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createVitalSigns, getVitalSigns, getLastWeek, probabilisticEvent } from "../controllers/VitalSigns.js";
+import { createVitalSigns, getVitalSigns, probabilisticEvent, promedio } from "../controllers/VitalSigns.js";
 import authorization from "../middleware/auth.jwt.js";
 const routerSigns = Router();
 
 
 routerSigns.post("/create", createVitalSigns); 
 routerSigns.get("/getAll",authorization,getVitalSigns); //protegiendo si hay token en la cookie
-routerSigns.get("/lastWeek",getLastWeek);
-routerSigns.get("/prueba", probabilisticEvent)
+routerSigns.get("/event", probabilisticEvent)
+routerSigns.get("/graph", promedio)
 
 
 export default routerSigns;
